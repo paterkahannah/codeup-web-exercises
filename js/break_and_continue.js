08341d5f64cfd7dfda9c1e670a037612581256c2
Hannah Paterka <paterkahannah@gmail.com>
2021-04-03 11:41:47 -0500

    notes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,11 +1,14 @@
 "use strict";
 
+// this prompts user for a number between 1-50
 var userNum = prompt('enter an odd number between 1-50');
 
+// this function verifies the number is between 1-50 and positive
 function check(num) {
     return num >= 1 && num <= 50 && num % 2 !== 0;
 }
 
+// this loop repeats the above prompt if the customer enters a number which doesn't meet the requirements
 while (true) {
     if (check(userNum)) {
         break;
@@ -13,6 +16,7 @@ while (true) {
     userNum = parseInt(prompt("Give me an odd number between 1 and 50 please?"));
 }
 
+// this loop outputs all odd numbers between 1-50, skipping the even numbers, and outputting a special line for the number the customer entered
 for (var i = 1; i <= 49; i++) {
     if (i % 2 === 0) {
         continue;
