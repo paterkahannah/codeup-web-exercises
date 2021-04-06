51dd0e6fcf36ca651e305d4c045dfabd6d48b1d2
Hannah Paterka <paterkahannah@gmail.com>
2021-04-06 16:46:06 -0500

    finished circle exercise

--- js/circle.js
+++ js/circle.js
@@ -3,25 +3,27 @@
 
     // create a circle object
     var circle = {
-        radius: 5,
+        radius: 3,
 
         getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
+            // TODO: complete this method / hint: area = pi * radius^2
             // TODO: return the proper value
 
-            return Math.PI * this.radius^2;
+            return Math.PI * (this.radius * this.radius);
+            },
 
-        },
+
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            var circleArea = this.getArea();
             if (doRounding) {
-                Math.round(circle)
+                circleArea = Math.round(circleArea)
             }
+
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circleArea);
         }
     };
 
@@ -34,9 +36,12 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+    circle.radius = 5;
+
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 })();
+
