eb397bc0f5b5c6ff09f9f54d2038f8aa69b8eceb
Hannah Paterka <paterkahannah@gmail.com>
2021-04-06 17:05:38 -0500

    improved code

--- js/external.js
+++ js/external.js
@@ -11,22 +11,40 @@ var response = prompt("What's your favorite color?");
 alert("Great, " + response + " is my favorite color too.");
 
 // part 3.1
-var mermaid = parseFloat(prompt('How many days did you rent Little Mermaid?'));
-var bear = parseFloat(prompt("How many days did you rent Brother Bear?"));
-var hercules = parseFloat(prompt("How many days did you rent Hercules?"));
+// var mermaid = parseFloat(prompt('How many days did you rent Little Mermaid?'));
+while (isNaN(mermaid)){
+    var mermaid = parseFloat(prompt('How many days did you rent Little Mermaid?'));
+}
+while (isNaN(bear)) {
+    var bear = parseFloat(prompt("How many days did you rent Brother Bear?"));
+}
+while (isNaN(hercules)) {
+    var hercules = parseFloat(prompt("How many days did you rent Hercules?"));
+}
 
 var total = alert("Total cost for the rentals was $" + ((mermaid + bear + hercules) * 3));
 
-// part 3.2
-var google = parseFloat(prompt('How many hours did you work at Google?'));
-var amazon = parseFloat(prompt("How many hours did you work at Amazon?"));
-var facebook = parseFloat(prompt("How many hours did you work at Facebook?"));
+// while (counter <= 65536) {
+//     console.log(counter);
+//     counter = counter * 2;
+// }
 
+
+// part 3.2
+while (isNaN(google)) {
+    var google = parseFloat(prompt('How many hours did you work at Google?'));
+}
+while (isNaN(amazon)) {
+    var amazon = parseFloat(prompt("How many hours did you work at Amazon?"));
+}
+while (isNaN(facebook)) {
+    var facebook = parseFloat(prompt("How many hours did you work at Facebook?"));
+}
 var googleRate = parseFloat(400);
 var amazonRate = parseFloat(380);
 var facebookRate = parseFloat(350);
 
-var totals = alert("Your total paycheck is $" + ((google * googleRate) + (amazon * amazonRate) + (facebook * facebookRate)));
+var totals = alert("Your total paycheck is $" + ((google * googleRate) + (amazon * amazonRate) + (facebook * facebookRate)).toFixed(2));
 
 // part 3.3
 var classMax = 40;
@@ -41,7 +59,6 @@ if (classOpen && noConflict === true) {
 }
 
 // part 3.4
-var offerNotExpired = true;
 var question = confirm('Are you a member or have you bought 2 items?');
 
 if (question === true) {
