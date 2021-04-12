f9c6ffab117301cddf17bb0b9d25b3cc3810262c
Hannah Paterka <paterkahannah@gmail.com>
2021-04-11 22:42:30 -0500

    loopy

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,20 +3,15 @@
 // prompts user for a number between 1-50
 var userNum = prompt('Please enter an odd number between 1-50.');
 
-// function verifies number is between 1-50 and positive
-function check(num) {
-    return num >= 1 && num <= 50 && num % 2 !== 0;
-}
-
-// this loop repeats the prompt until user enters a number that satisifies the requirements
+// repeats the prompt until user enters a number that satisfies the requirements
 while (true) {
-    if (check(userNum)) {
+    if (userNum >= 1 && userNum <= 50 && userNum % 2 !== 0) {
         break;
     }
     userNum = prompt("I'm sorry, please enter an odd number between 1 and 50.");
 }
 
-// this loop outputs all odd numbers between 1-50, skipping the even numbers, and outputting a special line for the number the customer entered
+// outputs all odd numbers between 1-50, skipping the even numbers, and outputting a special line for the number the customer entered
 for (var i = 1; i <= 49; i++) {
     if (i % 2 === 0) {
         continue;
