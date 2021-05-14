0bd2d379d8b85c86af9bf3ef90426112185a2df9
Hannah Paterka <paterkahannah@gmail.com>
2021-05-14 10:31:34 -0500

    completed exercise

---
+++ js/es6.js
@@ -0,0 +1,88 @@
+/*
+ * Complete the TODO items below
+ */
+const users = [
+  {
+    name: 'zach',
+    email: 'zach@codeup.com',
+    languages: ['javascript', 'bash']
+  },
+  {
+    name: 'ryan',
+    email: 'ryan@codeup.com',
+    languages: ['clojure', 'javascript']
+  },
+  {
+    name: 'luis',
+    email: 'luis@codeup.com',
+    languages: ['java', 'scala', 'php']
+  },
+  {
+    name: 'fernando',
+    email: 'fernando@codeup.com',
+    languages: ['java', 'php', 'sql']
+  },
+  {
+    name: 'justin',
+    email: 'justin@codeup.com',
+    languages: ['html', 'css', 'javascript', 'php']
+  }
+];
+console.log(users);
+
+// TODO: fill in your name and email and add some programming languages you know to the languages array
+// TODO: replace the `var` keyword with `const`, then try to reassign a variable declared as `const`
+
+const name = 'HANNAH';
+const email = 'paterkahannah@gmail.com';
+const languages = ['html', 'css', 'javascript'];
+
+// TODO: rewrite the object literal using object property shorthand
+
+users.push({
+  name,
+  email,
+  languages
+});
+
+// TODO: replace `var` with `let` in the following variable declarations
+
+let emails = [];
+let names = [];
+
+// TODO: rewrite the following using arrow functions
+
+users.forEach(user => emails.push(user.email));
+users.forEach(user => names.push(user.name));
+console.log(emails);
+console.log(names);
+
+// TODO: replace `var` with `let` in the following declaration
+
+let developers = [];
+
+// TODO: rewrite the code below to use object destructuring assignment. Note that you can also use destructuring assignment in the function parameter definition
+// TODO: rewrite the assignment below to use template strings
+
+users.forEach(user => {
+const person = {name: user.name, email: user.email, languages: user.languages};
+const {name, email, languages} = person;
+
+developers.push(`${name} \'s email is ${email} ${name} knows ${languages.join(', ')}`);
+});
+console.log(developers);
+
+// TODO: Use `let` for the following variable
+
+let list = `<ul>\n`;
+
+// TODO: rewrite the following loop to use a for..of loop
+
+for (let developer of developers) {
+  list += `<li> ${developer} </li>\n`;
+}
+
+// TODO: rewrite the assignment below to use template strings
+
+list += `</ul>`;
+console.log(list);
