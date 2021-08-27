c22fb24f8532d09b3b5bb2064ab602e178a1a33a
Hannah Paterka <paterkahannah@gmail.com>
2021-08-26 23:45:28 -0500

    tweaks

--- js/circle.js
+++ js/circle.js
@@ -1,4 +1,4 @@
-(function() {
+{
     "use strict";
 
     // create a circle object
@@ -11,7 +11,6 @@
 
             return Math.PI * (this.radius * this.radius);
             },
-
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
@@ -44,5 +43,4 @@
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
-})();
-
+}
